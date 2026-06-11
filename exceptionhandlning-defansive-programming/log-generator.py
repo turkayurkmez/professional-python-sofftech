039f73acbc935c93ad8cc3488bc0f57395bb5705
türkay ürkmez <turkay.urkmez@outlook.com>
2026-06-11 11:03:30 +0300

    Add logGenerator class with logging configuration and logger retrieval method

--- exceptionhandlning-defansive-programming/log-generator.py
+++ exceptionhandlning-defansive-programming/log-generator.py
@@ -1,1 +1,21 @@
-class logGenerator
+import logging
+class logGenerator:
+
+    logger: None
+    def __init__(self):
+        #1. adım: Logging'i yapılandır.
+        logging.basicConfig(level=logging.DEBUG, 
+                            format="%(asctime)s | %(levelname)s | %(message)s",
+                            handlers=[
+                            logging.StreamHandler(), # console'a yaz
+                            logging.FileHandler("app.log") #dosyaya da yaz
+                            ])   
+        self.logger = logging.getLogger(__name__)
+
+    
+
+    def get_logger(self):
+        return self.logger
+
+
+   
